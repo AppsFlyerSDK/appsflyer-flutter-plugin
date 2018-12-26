5b76176c8be04710cfebd3d6991163e5606e68e2
Maxim <maxim@appsflyer.com>
2018-12-26 15:13:33 +0200

    removed redundant code

--- lib/appsflyer_sdk.dart
+++ lib/appsflyer_sdk.dart
@@ -11,7 +11,7 @@ import 'dart:core';
 class AppsflyerSdk {
   static const MethodChannel _channel =
       const MethodChannel(AppsflyerConstants.AF_METHOD_CHANNEL);
-  static Stream<dynamic> _afStream;
+  
   static StreamController _afGCDStreamController;
   static StreamController _afOpenAttributionStreamController;
 
