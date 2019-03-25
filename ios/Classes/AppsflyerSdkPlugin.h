37f78396421fe6dbfe70e9786d5af8c3221fe23c
shahar cohen <shahar.cohen@appsflyer.com>
2019-03-25 12:05:01 +0200

    Updated to 1.1.1 + fix to iOS dependency problem

--- ios/Classes/AppsflyerSdkPlugin.h
+++ ios/Classes/AppsflyerSdkPlugin.h
@@ -1,5 +1,5 @@
 #import <Flutter/Flutter.h>
-#import <AppsFlyerFramework/AppsFlyerLib/AppsFlyerTracker.h>
+#import <AppsFlyerLib/AppsFlyerTracker.h>
 
 
 @interface AppsflyerSdkPlugin : NSObject<FlutterPlugin, AppsFlyerTrackerDelegate>
