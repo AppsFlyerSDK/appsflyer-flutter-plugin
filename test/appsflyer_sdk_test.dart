70fa73f50eb3df0e5bb4d84a574fea280b47442a
shahar <shahar.cohen@appsflyer.com>
2020-10-13 13:21:19 +0300

    add api tests

--- test/appsflyer_sdk_test.dart
+++ test/appsflyer_sdk_test.dart
@@ -6,41 +6,98 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   AppsflyerSdk instance;
+  String selectedMethod = "";
 
   group('AppsFlyerSdk', () {
-    final List<MethodCall> log = <MethodCall>[];
     const MethodChannel methodChannel = MethodChannel('af-api');
     const EventChannel eventChannel = EventChannel('af-events');
     // const AppsflyerSdk appsflyerSdk = AppsflyerSdk(options);
 
     setUp(() {
-      methodChannel.setMethodCallHandler((MethodCall methodCall) async {
-        log.add(methodCall);
-        return '';
+      methodChannel.setMockMethodCallHandler((MethodCall methodCall) async {
+        String method = methodCall.method;
+        switch (method) {
+          case 'setOneLinkCustomDomain':
+          case 'logCrossPromotionAndOpenStore':
+          case 'logCrossPromotionImpression':
+          case 'setAppInviteOneLinkID':
+          case 'generateInviteLink':
+          case 'setSharingFilterForAllPartners':
+          case 'setSharingFilter':
+          case 'getSDKVersion':
+          case 'getAppsFlyerUID':
+          case 'validateAndLogInAppPurchase':
+          case 'setMinTimeBetweenSessions':
+          case 'getHostPrefix':
+          case 'getHostName':
+          case 'setCollectIMEI':
+          case 'setCollectAndroidId':
+          case 'setUserEmailsWithCryptType':
+          case 'setUserEmails':
+          case 'setAdditionalData':
+          case 'waitForCustomerUserId':
+          case 'setCustomerUserId':
+          case 'enableLocationCollection':
+          case 'setAndroidIdData':
+          case 'setImeiData':
+          case 'updateServerUninstallToken':
+          case 'stop':
+          case 'setIsUpdate':
+          case 'setCurrencyCode':
+          case 'setHost':
+          case 'logEvent':
+          case 'initSdk':
+            selectedMethod = methodCall.method;
+            break;
+        }
       });
-
-      log.clear();
 
       //test map options way
       instance = AppsflyerSdk.private(methodChannel, eventChannel,
           mapOptions: {'afDevKey': 'sdfhj2342cx'});
     });
 
-    test('check initSdk call', () async {
-      methodChannel.setMockMethodCallHandler((MethodCall methodCall) async {
-        switch (methodCall.method) {
-          case 'initSdk':
-            return 'SUCCESS';
-            break;
-        }
-        return 'FAIL';
-      });
+    tearDown(() {
+      methodChannel.setMockMethodCallHandler(null);
+    });
 
+    test('check initSdk call', () async {
       dynamic res = await instance.initSdk(
         registerConversionDataCallback: true,
         registerOnAppOpenAttributionCallback: true,
       );
-      expect(res, 'SUCCESS');
+
+      expect('initSdk', selectedMethod);
+    });
+
+    test('check logEvent call', () async {
+      await instance.logEvent("eventName", {"key": "val"});
+
+      expect('logEvent', selectedMethod);
+    });
+
+    test('check setHost call', () async {
+      instance.setHost("", "");
+
+      expect('setHost', selectedMethod);
+    });
+
+    test('check setCurrencyCode call', () async {
+      instance.setCurrencyCode("currencyCode");
+
+      expect('setCurrencyCode', selectedMethod);
+    });
+
+    test('check setIsUpdate call', () async {
+      instance.setIsUpdate(true);
+
+      expect('setIsUpdate', selectedMethod);
+    });
+
+    test('check stop call', () async {
+      instance.stop(true);
+
+      expect('stop', selectedMethod);
     });
   });
 }
