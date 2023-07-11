dbcb57e528d6553d7e568be55f188b6dd8a78248
gabriel mandler <gabriel.mandler@appsflyer.com>
2023-07-11 12:04:49 +0300

    update appdelegate for att

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import AppsFlyerLib
+import AppTrackingTransparency
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,27 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      AppsFlyerLib.shared().waitForATTUserAuthorization(timeoutInterval: 60)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+        if  #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { (status) in
+              switch status {
+              case .denied:
+                  print("AuthorizationSatus is denied")
+              case .notDetermined:
+                  print("AuthorizationSatus is notDetermined")
+              case .restricted:
+                  print("AuthorizationSatus is restricted")
+              case .authorized:
+                  print("AuthorizationSatus is authorized")
+              @unknown default:
+                  fatalError("Invalid authorization status")
+              }
+            }
+          }
+        }
 }
