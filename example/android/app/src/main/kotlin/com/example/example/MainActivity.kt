e30079782c6132cbe41613ffcf0b5cd52af01110
Amit Levy <Amit.Levy@appsflyer.com>
2023-08-24 16:30:12 +0300

    Updated to SDK 6.12.2 -

---
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appsflyer.appsflyersdkexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/ios/Runner/AFSDKMock.h
@@ -0,0 +1,17 @@
+//
+//  AFSDKMock.h
+//  Runner
+//
+//  Created by Shahar Cohen on 12/09/2019.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AFSDKMock : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
