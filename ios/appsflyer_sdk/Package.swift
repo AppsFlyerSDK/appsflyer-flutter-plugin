c6ac8605270fb62b3276ae3a05e2743650c1e1d7
alejandro-all-win-software <alejandro@all-win.software>
2025-08-13 10:47:44 -0600

    chore: code refactor

--- ios/appsflyer_sdk/Package.swift
+++ ios/appsflyer_sdk/Package.swift
@@ -2,6 +2,31 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import Foundation
+
+let purchaseConnectorEnabled = ProcessInfo.processInfo.environment["ENABLE_PURCHASE_CONNECTOR"] == "1"
+
+var dependencies: [Package.Dependency] = [
+    .package(
+        url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework",
+        exact: "6.17.1"
+    )
+]
+
+var appsflyerSdkDependencies: [Target.Dependency] = [
+    .product(name: "AppsFlyerLib", package: "AppsFlyerFramework")
+]
+
+if purchaseConnectorEnabled {
+    dependencies.append(
+        .package(
+            url: "https://github.com/AppsFlyerSDK/appsflyer-apple-purchase-connector",
+            exact: "6.17.1"
+        )
+    )
+
+    appsflyerSdkDependencies.append("purchase_connector")
+}
 
 let package = Package(
     name: "appsflyer_sdk",
@@ -11,18 +36,21 @@ let package = Package(
     products: [
         .library(name: "appsflyer-sdk", type: .static, targets: ["appsflyer_sdk"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", .upToNextMajor(from: "6.15.3")),
-    ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "appsflyer_sdk",
-            dependencies: [
-                .product(name: "AppsFlyerLib", package: "AppsFlyerFramework")
-            ],
+            dependencies: appsflyerSdkDependencies,
             cSettings: [
                 .headerSearchPath("include/appsflyer_sdk")
             ]
-        )
-    ]
+        ),
+        purchaseConnectorEnabled ?
+            .target(
+                name: "purchase_connector",
+                dependencies: [
+                    .product(name: "PurchaseConnector", package: "appsflyer-apple-purchase-connector")
+                ]
+            ) : nil
+    ].compactMap { $0 }
 )
