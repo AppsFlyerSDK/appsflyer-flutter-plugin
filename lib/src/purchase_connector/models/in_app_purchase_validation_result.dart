a182ee9cd7997a4dfb181233a47d4ea19f9529ea
Dani-Koza-AF <dani.ko@appsflyer.com>
2025-10-15 09:39:22 +0300

    format code with dart format

--- lib/src/purchase_connector/models/in_app_purchase_validation_result.dart
+++ lib/src/purchase_connector/models/in_app_purchase_validation_result.dart
@@ -2,32 +2,28 @@ part of appsflyer_sdk;
 
 @JsonSerializable()
 class InAppPurchaseValidationResult {
-
   bool success;
   ProductPurchase? productPurchase;
   ValidationFailureData? failureData;
 
   InAppPurchaseValidationResult(
-      this.success,
-      this.productPurchase,
-      this.failureData
-      );
+      this.success, this.productPurchase, this.failureData);
 
-
-
-  factory InAppPurchaseValidationResult.fromJson(Map<String, dynamic> json) => _$InAppPurchaseValidationResultFromJson(json);
+  factory InAppPurchaseValidationResult.fromJson(Map<String, dynamic> json) =>
+      _$InAppPurchaseValidationResultFromJson(json);
 
   Map<String, dynamic> toJson() => _$InAppPurchaseValidationResultToJson(this);
-
 }
 
 @JsonSerializable()
-class InAppPurchaseValidationResultMap{
+class InAppPurchaseValidationResultMap {
   Map<String, InAppPurchaseValidationResult> result;
 
   InAppPurchaseValidationResultMap(this.result);
-  factory InAppPurchaseValidationResultMap.fromJson(Map<String, dynamic> json) => _$InAppPurchaseValidationResultMapFromJson(json);
+  factory InAppPurchaseValidationResultMap.fromJson(
+          Map<String, dynamic> json) =>
+      _$InAppPurchaseValidationResultMapFromJson(json);
 
-  Map<String, dynamic> toJson() => _$InAppPurchaseValidationResultMapToJson(this);
-
+  Map<String, dynamic> toJson() =>
+      _$InAppPurchaseValidationResultMapToJson(this);
 }
