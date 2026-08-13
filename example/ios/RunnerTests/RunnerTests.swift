486424874cbf7d664aefd765a7d23637471f3453
Andrii Dudka <andrii.dudka@appsflyer.com>
2026-08-13 16:57:27 +0300

    [DELIVERY-125863] Fix exclude-connector typo and remove empty iOS test placeholder

--- example/ios/RunnerTests/RunnerTests.swift
+++ example/ios/RunnerTests/RunnerTests.swift
@@ -1,15 +1,4 @@
-import Flutter
-import UIKit
 import XCTest
-
-class RunnerTests: XCTestCase {
-
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
-  }
-
-}
 
 /// Pins the Foundation behavior the iOS plugin relies on instead of an Objective-C `@try`/`@catch`
 /// boundary around RPC dispatch.
